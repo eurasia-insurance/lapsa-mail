f733a8f74bbd3f1fabf3b38f22573b4e340c39c8
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-04-12 11:30:42 +0600

    Тест на базовый функционал Factory и на отправления разнесены по разным классам.

--- impl/src/test/java/com/metrobank/mail/test/BasicTestCase.java
+++ impl/src/test/java/com/metrobank/mail/test/BasicTestCase.java
@@ -2,61 +2,26 @@ package com.metrobank.mail.test;
 
 import static org.junit.Assert.*;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Properties;
 
-import javax.mail.Authenticator;
-import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import com.lapsa.mailutil.InvalidMessageException;
 import com.lapsa.mailutil.MailException;
 import com.lapsa.mailutil.MailFactory;
-import com.lapsa.mailutil.MailMessage;
 import com.lapsa.mailutil.MailMessageBuilder;
-import com.lapsa.mailutil.MailMessagePart;
 import com.lapsa.mailutil.MailSender;
 import com.lapsa.mailutil.MailService;
 
 public class BasicTestCase {
 
-    private static final String MAIL_TEST_USER = "lapsa.test@gmail.com";
-    private static final String MAIL_TEST_PASSWORD = "SuperPuper2016";
-
-    private static final String MAIL_TEST_HOST = "smtp.gmail.com";
-
-    private static final String MAIL_TEST_FROM_ADDRESS = MAIL_TEST_USER;
-    private static final String MAIL_TEST_RECIPIENT_ADDRESS = MAIL_TEST_USER;
-
-    private static final String PATH_TO_TEST_RESOURCE = "policy_sample.jpg";
-    private static final String PATH_TO_TEST_FILE = "src/test/resources/policy_sample.jpg";
-
     private static Session session;
 
     @BeforeClass
     public static void prepareSession() {
-	Properties prop = new Properties();
-
-	prop.put("mail.transport.protocol", "smtps");
-	prop.put("mail.smtps.class", "com.sun.mail.smtp.SMTPSSLTransport");
-	prop.put("mail.smtps.auth", "true");
-	prop.put("mail.smtps.host", MAIL_TEST_HOST);
-	prop.put("mail.smtps.port", "465");
-	prop.put("mail.smtps.starttls.enable", "true");
-	prop.put("mail.from", MAIL_TEST_FROM_ADDRESS);
-	prop.put("mail.debug", true);
-	Authenticator a = new Authenticator() {
-	    public PasswordAuthentication getPasswordAuthentication() {
-		return new PasswordAuthentication(MAIL_TEST_USER, MAIL_TEST_PASSWORD);
-	    }
-	};
-	session = Session.getInstance(prop, a);
+	session = Session.getDefaultInstance(new Properties());
     }
 
     @Test
@@ -87,145 +52,4 @@ public class BasicTestCase {
 	MailMessageBuilder mmb = mh.createBuilder();
 	assertNotNull(mmb);
     }
-
-    @Test(expected = InvalidMessageException.class)
-    public void testEmptyRecipientsException() throws MailException, InvalidMessageException {
-	MailFactory mhf = MailFactory.getDefaultMailFactory();
-	MailService mh = mhf.getService(session);
-	MailMessageBuilder mmb = mh.createBuilder();
-
-	MailMessage message = mmb.createMessage();
-	MailMessagePart part = mmb.createTextPart("Test message");
-	message.addPart(part);
-	message.setSubject("Test message");
-	MailSender sender = mh.createSender();
-	sender.send(message);
-    }
-
-    @Test(expected = InvalidMessageException.class)
-    public void testEmptyBodyException() throws MailException, InvalidMessageException {
-	MailFactory mhf = MailFactory.getDefaultMailFactory();
-	MailService mh = mhf.getService(session);
-	MailMessageBuilder mmb = mh.createBuilder();
-
-	MailMessage message = mmb.createMessage();
-	message.addTORecipient(mmb.createAddress(MAIL_TEST_RECIPIENT_ADDRESS));
-	message.setSubject("Test message");
-	MailSender sender = mh.createSender();
-	sender.send(message);
-    }
-
-    @Test
-    public void testSend() throws MailException, InvalidMessageException {
-	MailFactory mhf = MailFactory.getDefaultMailFactory();
-	MailService mh = mhf.getService(session);
-	MailSender sender = mh.createSender();
-	MailMessageBuilder mmb = mh.createBuilder();
-
-	MailMessage message = mmb.createMessage();
-	message.addTORecipient(mmb.createAddress(MAIL_TEST_RECIPIENT_ADDRESS));
-	message.setSubject("testSend");
-	message.addPart(mmb.createTextPart("testSend"));
-	sender.send(message);
-    }
-
-    @Test
-    public void testSendAlwaysCopyTo() throws MailException, InvalidMessageException {
-	MailFactory mhf = MailFactory.getDefaultMailFactory();
-	MailService mh = mhf.getService(session);
-	MailSender sender = mh.createSender();
-	MailMessageBuilder mmb = mh.createBuilder();
-
-	MailMessage message = mmb.createMessage();
-	message.addTORecipient(mmb.createAddress(MAIL_TEST_RECIPIENT_ADDRESS));
-	message.setSubject("testSendAlwaysCopyTo");
-	message.addPart(mmb.createTextPart("testSendAlwaysCopyTo"));
-	sender.setAlwaysBlindCopyTo(mmb.createAddress(MAIL_TEST_RECIPIENT_ADDRESS));
-	sender.send(message);
-    }
-
-    @Test
-    public void testSendImage_InputStream() throws MailException, IOException, InvalidMessageException {
-	InputStream is = null;
-	try {
-	    MailFactory mf = MailFactory.getDefaultMailFactory();
-	    MailService ms = mf.getService(session);
-	    MailMessageBuilder builder = ms.createBuilder();
-	    MailMessage message = builder.createMessage();
-	    message.addTORecipient(builder.createAddress(MAIL_TEST_RECIPIENT_ADDRESS));
-	    message.setSubject("testSendImage_InputStream");
-	    is = testFileInputStream();
-	    message.addPart(builder.createStreamPart("PICTURE", "image/jpeg", is));
-	    MailSender sender = ms.createSender();
-	    sender.send(message);
-	} finally {
-	    if (is != null)
-		is.close();
-	}
-    }
-
-    @Test
-    public void testSendImage_File() throws MailException, IOException, InvalidMessageException {
-	InputStream is = null;
-	try {
-	    MailFactory mf = MailFactory.getDefaultMailFactory();
-	    MailService ms = mf.getService(session);
-	    MailMessageBuilder builder = ms.createBuilder();
-	    MailMessage message = builder.createMessage();
-	    message.addTORecipient(builder.createAddress(MAIL_TEST_RECIPIENT_ADDRESS));
-	    message.setSubject("testSendImage_File");
-	    MailMessagePart part = builder.createFilePart(new File(PATH_TO_TEST_FILE));
-	    message.addPart(part);
-	    MailSender sender = ms.createSender();
-	    sender.send(message);
-	} finally {
-	    if (is != null)
-		is.close();
-	}
-    }
-
-    @Test
-    public void testSendImage_Bytes() throws MailException, IOException, InvalidMessageException {
-	MailFactory mf = MailFactory.getDefaultMailFactory();
-	MailService ms = mf.getService(session);
-	MailMessageBuilder builder = ms.createBuilder();
-	MailMessage message = builder.createMessage();
-	message.addTORecipient(builder.createAddress(MAIL_TEST_RECIPIENT_ADDRESS));
-	message.setSubject("testSendImage_Bytes");
-	byte[] bytes = testFileBytes();
-	message.addPart(builder.createByteArrayPart("PICTURE.jpg", "image/jpeg", bytes));
-	MailSender sender = ms.createSender();
-	sender.send(message);
-    }
-
-    private InputStream testFileInputStream() {
-	InputStream is = this.getClass().getClassLoader().getResourceAsStream(PATH_TO_TEST_RESOURCE);
-	return is;
-    }
-
-    private byte[] testFileBytes() throws IOException {
-	InputStream is = null;
-	ByteArrayOutputStream baos = null;
-	try {
-	    is = testFileInputStream();
-	    byte[] buff = new byte[256];
-	    int readed;
-	    baos = new ByteArrayOutputStream();
-	    while ((readed = is.read(buff)) != -1)
-		baos.write(buff, 0, readed);
-	    return baos.toByteArray();
-	} finally {
-	    if (baos != null)
-		try {
-		    baos.close();
-		} catch (IOException e) {
-		}
-	    if (is != null)
-		try {
-		    baos.close();
-		} catch (IOException e) {
-		}
-	}
-    }
-
 }
